69d930f5c76b916853f454788249ebf462dd5557
Fabio Y. Chinen <133177634+fabioychinen@users.noreply.github.com>
2023-09-20 09:37:04 -0300

    Update constants.dart

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,9 +1,3 @@
-class Constants {
-  static const userFavoritesUrl =
-      'https://touf-shop-default-rtdb.firebaseio.com/userFavorites';
-  static const productBaseUrl =
-      'https://touf-shop-default-rtdb.firebaseio.com/products';
-  static const orderBaseUrl =
-      'https://touf-shop-default-rtdb.firebaseio.com/orders';
+
   static const webApiKey = 'AIzaSyB8I9aUbu2d4YNjgVQ-yIwBfnbrtelXO28';
 }
